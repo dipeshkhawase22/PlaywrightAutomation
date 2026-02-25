02d660ab0474643c6907ededdd53b8893a236849
Dipesh Khawase <dipeshkhawase@Dipeshs-MacBook-Air.local>
2026-02-25 23:08:41 +0530

    Add assignment grade calculator script

---
+++ Statements/assignment_GardeCalculator.js
@@ -0,0 +1,13 @@
+let garde = 79;
+
+if (garde > 90){
+    console.log("You got A grade!")
+}else if (garde >= 80 && garde <= 90){
+    console.log("You got B grade!")
+}else if (garde >= 70 && garde < 80){
+    console.log("You got C grade!")
+}else if (garde >= 60 && garde < 70 ){
+    console.log("You got D grade!")
+}else{
+    console.log("You got F grade!")
+}
